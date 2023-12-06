7a5b8d9f69bdab980cf4f442d1005716d63a3551
Trismegisto <rodrigoboncardoso@yahoo.com>
2023-12-05 23:08:01 -0300

    atlz_0.3 - microstart class construction

--- lib/MicroStart/microstart.cpp
+++ lib/MicroStart/microstart.cpp
@@ -1,3 +1,15 @@
 #include <Arduino.h>
 #include "microstart.h"
 
+Start::Start(int inputPin){
+    this->inputPin = inputPin;
+};
+
+void Start::updateStartState(){
+    if(digitalRead(inputPin) == 1){
+        this->state = startSate::sState::START;
+    }
+    else if(!digitalRead(inputPin) == 0){
+        this->state = startSate::sState::STOP;
+    }
+}
--- lib/MicroStart/microstart.h
+++ lib/MicroStart/microstart.h
@@ -1,6 +1,21 @@
 #ifndef MICROSTART_H
-#define MOCROSTART_H
+#define MICROSTART_H
 
+namespace startSate{
+    enum sState{
+        STOP,
+        START
+    };
+}
 
+class Start{
+    public:
+        int inputPin;
+        startSate::sState state;
+
+        Start(int inputPin);
+        void updateStartState();
+        
+};
 
 #endif
