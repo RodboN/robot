78cdd666c923f8d02b59670813d3690682659a4d
Trismegisto <rodrigoboncardoso@yahoo.com>
2024-02-18 17:42:55 -0300

    robot.cpp creation

---
+++ lib/Robot/robot.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#include "robot.h"
+#include "digital_sensor.h"
+#include "vision.h"
+
+Robot::Robot();
+
+void Robot::readSensor(){
+    this->front.DigitalSensor::readDigitalSensor();
+    this->full_left.DigitalSensor::readDigitalSensor();
+    this->left.DigitalSensor::readDigitalSensor();
+    this->full_right.DigitalSensor::readDigitalSensor();
+    this->right.DigitalSensor::readDigitalSensor();
+}
+
+void Robot::update(){
+    this->vision.Vision::updateEnemyPosition(DigitalSensor &front, DigitalSensor &full_left, DigitalSensor &left, DigitalSensor &full_right, DigitalSensor &right);
+}
